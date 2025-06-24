b0d4735893edcb96316e88a66bde2d05ec372af8
Michal Mordarski <mmordarski4@gmail.com>
2025-06-24 16:27:44 +0200

    Add function to display shopping list

--- shopping-list.py
+++ shopping-list.py
@@ -3,3 +3,8 @@ shopping_list = {
     'miesny': ['cielecina', 'jagniecina', 'baranina'],
     'spozywczy': ['mleko', 'ser', 'jajka']
 }
+count = 0
+for shop, products in shopping_list.items():
+    print(f"Idę do {shop}, kupuję tu następujące rzeczy: {products}")
+    count += len(products)
+print(f"W sumie kupuję {count} produktów.")
