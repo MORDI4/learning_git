025e2e2b0a8a22a711e234c6fe1185bdd88ee394
Michał <116754598+MORDI4@users.noreply.github.com>
2025-06-24 23:43:20 +0200

    Update display of count shopping list items

--- shopping-list.py
+++ shopping-list.py
@@ -7,4 +7,4 @@ count = 0
 for shop, products in shopping_list.items():
     print(f"Idę do {shop.capitalize()}, kupuję tu następujące rzeczy: {[i.capitalize() for i in products]}")
     count += len(products)
-print(f"W sumie kupuję {count} produktów.")
+print(f"W sumie kupuję tyle produktów: {count}")
